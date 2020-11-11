35eddfb4e8160085824a93151e63e1f5201280c1
Wesley Tokoi <tokoiwesley@gmail.com>
2020-11-11 11:25:31 +0300

    Set up the database

--- mysite/settings.py
+++ mysite/settings.py
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mysite_test',
+        'USER': 'wesley',
+        'PASSWORD': 'wesley',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
