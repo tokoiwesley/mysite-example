956da0910f933353c40b2a7cd9ee4809de3a2216
Wesley Tokoi <tokoiwesley@gmail.com>
2020-11-16 09:10:34 +0300

    Add related objects to the Question form

--- polls/admin.py
+++ polls/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 
 # Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date Information', {'fields': ['pub_date']})
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']})
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
